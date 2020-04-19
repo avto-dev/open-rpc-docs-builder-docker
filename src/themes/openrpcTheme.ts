6b7cb198528c36997b889a215aba4ff1a9c1ebb8
jetexe <jetexe2@gmail.com>
2020-04-19 20:30:02 +0500

    change indent size

--- src/themes/openrpcTheme.ts
+++ src/themes/openrpcTheme.ts
@@ -2,92 +2,92 @@ import {createMuiTheme} from "@material-ui/core/styles";
 import grey from "@material-ui/core/colors/grey";
 
 export const lightTheme = createMuiTheme({
-    props: {
-        MuiAppBar: {
-            position: "sticky",
-        },
-        MuiCard: {
-            elevation: 0,
-        },
+  props: {
+    MuiAppBar: {
+      position: "sticky",
     },
-    overrides: {
-        MuiPaper: {
-            root: {
-                zIndex: 1,
-                opacity: 1,
-            },
-        },
-        MuiToolbar: {
-            root: {
-                background: "transparent !important",
-            },
-        },
-        MuiAppBar: {
-            root: {
-                backgroundColor: "transparent !important",
-            },
-            colorDefault: {
-                background: "transparent !important",
-            },
-            colorPrimary: {
-                background: "transparent !important",
-            },
-        },
+    MuiCard: {
+      elevation: 0,
     },
-    palette: {
-        background: {
-            default: "#fff",
-        },
+  },
+  overrides: {
+    MuiPaper: {
+      root: {
+        zIndex: 1,
+        opacity: 1,
+      },
     },
+    MuiToolbar: {
+      root: {
+        background: "transparent !important",
+      },
+    },
+    MuiAppBar: {
+      root: {
+        backgroundColor: "transparent !important",
+      },
+      colorDefault: {
+        background: "transparent !important",
+      },
+      colorPrimary: {
+        background: "transparent !important",
+      },
+    },
+  },
+  palette: {
+    background: {
+      default: "#fff",
+    },
+  },
 });
 
 export const darkTheme = createMuiTheme({
-    props: {
-        MuiAppBar: {
-            position: "sticky",
-        },
+  props: {
+    MuiAppBar: {
+      position: "sticky",
     },
-    palette: {
-        type: "dark",
-        background: {
-            default: grey[900],
-            paper: grey[900],
-        },
+  },
+  palette: {
+    type: "dark",
+    background: {
+      default: grey[900],
+      paper: grey[900],
     },
-    overrides: {
-        MuiPaper: {
-            root: {
-                zIndex: 1,
-                opacity: 1,
-            },
-        },
-        MuiAppBar: {
-            root: {
-                background: "transparent !important",
-            },
-            colorPrimary: {
-                background: "transparent !important",
-            },
-        },
-        MuiToolbar: {
-            root: {
-                background: "transparent !important",
-            },
-        },
-        MuiTable: {
-            root: {
-                background: "transparent !important",
-            },
-        },
-        MuiTypography: {
-            root: {
-                color: grey[400],
-            },
-        },
+  },
+  overrides: {
+    MuiPaper: {
+      root: {
+        zIndex: 1,
+        opacity: 1,
+      },
     },
+    MuiAppBar: {
+      root: {
+        background: "transparent !important",
+      },
+      colorPrimary: {
+        background: "transparent !important",
+      },
+    },
+    MuiToolbar: {
+      root: {
+        background: "transparent !important",
+      },
+    },
+    MuiTable: {
+      root: {
+        background: "transparent !important",
+      },
+    },
+    MuiTypography: {
+      root: {
+        color: grey[400],
+      },
+    },
+  },
 });
 
 export default {
-    darkTheme,
-    lightTheme,
+  darkTheme,
+  lightTheme,
 };
